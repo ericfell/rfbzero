022430e5e8cf8ee01265a09fa548d2e1e4b33a9a
Eric Fell <efell@g.harvard.edu>
2023-08-27 20:20:46 -0400

    add test for redox_flow_cell.py method

--- tests/test_redox_flow_cell.py
+++ tests/test_redox_flow_cell.py
@@ -1,24 +1,49 @@
 import pytest
 
-from rfbzero.rfbzero.zeroD_model_1e_vs_1e import ZeroDModel
+from rfbzero.redox_flow_cell import ZeroDModel
 
-def test_ZeroDModel():
+
+def test_zerodmodel():
     """
     Tests for ZeroDmodel class methods
 
     """
 
+    """
     def test_current_direction():
         x = ZeroDModel.current_direction(True)
         y = ZeroDModel.current_direction(False)
         assert x == 1
         assert y == -1
 
-
     ######
     test_current_direction()
+    """
+    def test_cell_voltage():
+        ocv = 1.0
+        loss = 0.2
+
+        cell_v1 = ZeroDModel.cell_voltage(ocv, loss, True)
+        cell_v2 = ZeroDModel.cell_voltage(ocv, loss, False)
+
+        assert cell_v1 == 1.2
+        assert cell_v2 == 0.8
+
+    def test_state_of_charge():
+        r_cls = 0.2
+        o_cls = 0.8
+        r_ncls = 0.3
+        o_ncls = 0.7
+
+        soc_c, soc_n = ZeroDModel.state_of_charge(o_cls, r_cls, o_ncls, r_ncls)
+
+        assert soc_c == 20.0
+        assert soc_n == 30.0
+
+    #
+    test_cell_voltage()
+    test_state_of_charge()
     print('All test passed!')
 
-
 ######
-test_ZeroDModel()
+test_zerodmodel()
