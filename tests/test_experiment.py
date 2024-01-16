d98a4681a75fe447e9e47bd2cbcab7a3a01a34da
Eric Fell <efell@g.harvard.edu>
2024-01-15 21:26:49 -0500

    add tests for cc, cccv, cv protocols

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,5 +1,5 @@
 import pytest
-#import numpy as np
+import numpy as np
 
 from rfbzero.experiment import ConstantCurrent, ConstantCurrentConstantVoltage, ConstantVoltage
 from rfbzero.redox_flow_cell import ZeroDModel
@@ -29,9 +29,38 @@ class TestCyclingProtocolResults:
 
 class TestConstantCurrent:
 
-    @pytest.mark.skip(reason="not implemented yet")
     def test_cc(self):
-        raise NotImplementedError
+        # define the battery design parameters
+        cell = ZeroDModel(cls_volume=0.005,  # L
+                          ncls_volume=0.05,  # L
+                          cls_start_c_ox=0.01,  # M
+                          cls_start_c_red=0.01,  # M
+                          ncls_start_c_ox=0.01,  # M
+                          ncls_start_c_red=0.01,  # M
+                          init_ocv=0.0,  # V
+                          resistance=1.0,  # ohms
+                          k_0_cls=1e-3,  # cm/s
+                          k_0_ncls=1e-3,  # cm/s
+                          n_cls=1,  # electrons
+                          n_ncls=1,  # electrons
+                          )
+
+        # define degradation mechanisms
+        cross = Crossover(membrane_thickness=183, permeability_ox=5e-6, permeability_red=2e-6)
+
+        protocol = ConstantCurrent(voltage_limit_charge=0.2,  # V
+                                   voltage_limit_discharge=-0.2,  # V
+                                   current=0.05,  # A
+                                   )
+
+        # putting it all together
+        all_results = protocol.run(cell_model=cell,
+                                   duration=1000,  # cycle time to simulate (s)
+                                   crossover=cross,
+                                   )
+        expected = [4.734000000000096, 9.397499999999994, 9.417500000000018, 9.416500000000017, 9.414500000000015]
+        vals = all_results.half_cycle_capacity[:5]
+        assert np.isclose(vals, expected).all()
 
 
 class TestConstantVoltage:
@@ -72,15 +101,54 @@ class TestConstantVoltage:
                                    duration=1000,  # cycle time to simulate (s)
                                    )
         expected = [4.809806770737516, 9.616034709809167, 9.61843069453628, 9.611898460659235, 9.611890636955804]
-
-        assert all_results.half_cycle_capacity[:5] == expected
+        vals = all_results.half_cycle_capacity[:5]
+        assert np.isclose(vals, expected).all()
 
 
 class TestConstantCurrentConstantVoltage:
 
-    @pytest.mark.skip(reason="not implemented yet")
     def test_cccv(self):
-        raise NotImplementedError
+        # define the battery design parameters
+        cell = ZeroDModel(cls_volume=0.005,  # L
+                          ncls_volume=0.03,  # L
+                          cls_start_c_ox=0.01,  # M
+                          cls_start_c_red=0.01,  # M
+                          ncls_start_c_ox=0.01,  # M
+                          ncls_start_c_red=0.01,  # M
+                          init_ocv=1.1,  # V
+                          resistance=0.8,  # ohms
+                          k_0_cls=1e-3,  # cm/s
+                          k_0_ncls=1e-3,  # cm/s
+                          n_cls=1,  # electrons
+                          n_ncls=1,  # electrons
+                          )
+
+        # define degradation mechanisms
+        deg1 = ChemicalDegradation(rate_order=2, rate_constant=5e-5, species='red')
+
+        deg2 = AutoOxidation(rate_constant=1e-4)
+
+        # define crossover mechanisms
+        cross = Crossover(membrane_thickness=183, permeability_ox=5e-6, permeability_red=2e-6)
+
+        # define cycling protocol
+        protocol = ConstantCurrentConstantVoltage(
+            voltage_limit_charge=1.45,
+            voltage_limit_discharge=0.8,
+            current_cutoff_charge=0.005,
+            current_cutoff_discharge=-0.005,
+            current=0.1
+        )
+
+        # putting it all together
+        all_results = protocol.run(cell_model=cell,
+                                   duration=1000,  # cycle time to simulate (s)
+                                   degradation=MultiDegradationMechanism([deg1, deg2]),
+                                   crossover=cross,
+                                   )
+        expected = [4.898116190603088, 9.622472024194355, 9.719800105349496, 9.622272591351388, 9.725092303584894]
+        vals = all_results.half_cycle_capacity[:5]
+        assert np.isclose(vals, expected).all()
 
 
 
