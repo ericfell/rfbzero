6d26547e4387194148c916e8acb031768906e9a4
Eric Fell <efell@g.harvard.edu>
2024-01-15 19:49:12 -0500

    add tests

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,15 +1,14 @@
 import pytest
-import numpy as np
+#import numpy as np
 
-from rfbzero.experiment import CyclingProtocolResults, CyclingProtocol, ConstantCurrent, ConstantCurrentConstantVoltage
+from rfbzero.experiment import ConstantCurrent, ConstantCurrentConstantVoltage, ConstantVoltage
 from rfbzero.redox_flow_cell import ZeroDModel
+from rfbzero.degradation import ChemicalDegradation, AutoOxidation, AutoReduction, MultiDegradationMechanism#, Dimerization
+from rfbzero.crossover import Crossover
 
 
 class TestCyclingProtocolResults:
 
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_results_init(self):
-        raise NotImplementedError
 
     @pytest.mark.skip(reason="not implemented yet")
     def test_state_of_charge(self):
@@ -28,57 +27,61 @@ class TestCyclingProtocolResults:
         raise NotImplementedError
 
 
-class TestCyclingProtocol:
-
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_abstract_class_init(self):
-        raise NotImplementedError
-
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_abstract_class_run(self):
-        test_model = ZeroDModel(cls_volume=6, ncls_volume=0.01, cls_start_c_ox=0.01, cls_start_c_red=0.01,
-                                ncls_start_c_ox=0.01, ncls_start_c_red=0.01, init_ocv=1.0, resistance=1, k_0_cls=1e-3,
-                                k_0_ncls=1e-3)
-        with pytest.raises(NotImplementedError):
-            abs_class = CyclingProtocol(current=1, charge_first=True)
-            abs_class.run(duration=100, cell_model=test_model)
-
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_validate_model(self):
-        raise NotImplementedError
-
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_current_direction(self):
-        x = CyclingProtocol(current=1, charge_first=True)
-        y = CyclingProtocol(current=1, charge_first=False)
-        assert x.current_direction() == 1
-        assert y.current_direction() == -1
-
-
 class TestConstantCurrent:
 
     @pytest.mark.skip(reason="not implemented yet")
-    def test_cc_init(self):
+    def test_cc(self):
         raise NotImplementedError
 
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_cc_run(self):  # BIG ONE
-        raise NotImplementedError
+
+class TestConstantVoltage:
+
+    def test_cv1(self):
+        # define the battery design parameters
+        cell = ZeroDModel(cls_volume=0.005,  # L
+                          ncls_volume=0.05,  # L
+                          cls_start_c_ox=0.01,  # M
+                          cls_start_c_red=0.01,  # M
+                          ncls_start_c_ox=0.01,  # M
+                          ncls_start_c_red=0.01,  # M
+                          init_ocv=0.0,  # V
+                          resistance=1.0,  # ohms
+                          k_0_cls=1e-3,  # cm/s
+                          k_0_ncls=1e-3,  # cm/s
+                          n_cls=1,  # electrons
+                          n_ncls=1,  # electrons
+                          )
+
+        # define degradation mechanisms
+        deg = ChemicalDegradation(rate_order=1,
+                                  rate_constant=1e-5,  # 1/s
+                                  species='red',
+                                  )
+
+        protocol = ConstantCurrentConstantVoltage(
+            voltage_limit_charge=0.2,  # volts
+            voltage_limit_discharge=-0.2,  # volts
+            current_cutoff_charge=0.005,  # amps
+            current_cutoff_discharge=-0.005,  # amps
+            current=1.5,  # amps
+        )
+
+        # putting it all together
+        all_results = protocol.run(cell_model=cell,
+                                   degradation=deg,
+                                   duration=1000,  # cycle time to simulate (s)
+                                   )
+        expected = [4.809806770737516, 9.616034709809167, 9.61843069453628, 9.611898460659235, 9.611890636955804]
+
+        assert all_results.half_cycle_capacity[:5] == expected
 
 
 class TestConstantCurrentConstantVoltage:
 
     @pytest.mark.skip(reason="not implemented yet")
-    def test_cccv_init(self):
+    def test_cccv(self):
         raise NotImplementedError
 
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_cccv_run(self):  # BIG ONE
-        raise NotImplementedError
-
-    @pytest.mark.skip(reason="not implemented yet")
-    def test_get_min_current(self):
-        raise NotImplementedError
 
 
 
