209d95aed4776ae76da50aa90787af829154a2e0
Jeremy Fell <jeremyafell@gmail.com>
2024-01-22 00:45:20 -0800

    Update method calls in tests

--- tests/test_redox_flow_cell.py
+++ tests/test_redox_flow_cell.py
@@ -72,7 +72,7 @@ class TestClassRedoxFlowCell:
         cell = ZeroDModel(cls_volume=0.005, ncls_volume=0.01, cls_start_c_ox=0.01, cls_start_c_red=0.01,
                           ncls_start_c_ox=0.01, ncls_start_c_red=0.01, init_ocv=1.0, resistance=1, k_0_cls=1e-3,
                           k_0_ncls=1e-3, n_ncls=2)
-        i_0_cls, i_0_ncls = cell._exchange_current()
+        i_0_cls, i_0_ncls = cell.__exchange_current()
         assert np.isclose(i_0_cls, 0.12543093175)
         assert np.isclose(i_0_ncls, 0.25086186351)
 
@@ -82,7 +82,7 @@ class TestClassRedoxFlowCell:
                           ncls_start_c_ox=0.01, ncls_start_c_red=0.01, init_ocv=1.0, resistance=1, k_0_cls=1e-3,
                           k_0_ncls=1e-3, n_ncls=2)
 
-        i_lim = cell._limiting_current(limiting_c)
+        i_lim = cell.__limiting_current(limiting_c)
         assert np.isclose(i_lim, 77.1882656)
 
     def test_limiting_concentration(self):
@@ -90,11 +90,11 @@ class TestClassRedoxFlowCell:
                           ncls_start_c_ox=0.02, ncls_start_c_red=0.01, init_ocv=1.0, resistance=1, k_0_cls=1e-3,
                           k_0_ncls=1e-3, n_ncls=2)
 
-        i_lim_cls, i_lim_ncls = cell.limiting_concentration(True)
+        i_lim_cls, i_lim_ncls = cell._limiting_concentration(True)
         assert np.isclose(i_lim_cls, 3.85941328)
         assert np.isclose(i_lim_ncls, 7.71882656)
 
-        i_lim_cls, i_lim_ncls = cell.limiting_concentration(False)
+        i_lim_cls, i_lim_ncls = cell._limiting_concentration(False)
         assert np.isclose(i_lim_cls, 7.71882656)
         assert np.isclose(i_lim_ncls, 15.43765312)
 
@@ -105,7 +105,7 @@ class TestClassRedoxFlowCell:
         current = 1
         i_0_cls = 0.01
         i_0_ncls = 0.01
-        n_activation = cell._activation_overpotential(current, i_0_cls, i_0_ncls)
+        n_activation = cell.__activation_overpotential(current, i_0_cls, i_0_ncls)
         assert np.isclose(n_activation, 0.177392243)
 
     @pytest.mark.skip(reason="not implemented yet")
@@ -144,8 +144,8 @@ class TestClassRedoxFlowCell:
         ocv = 1.0
         loss = 0.2
 
-        cell_v1 = ZeroDModel.cell_voltage(ocv, loss, True)
-        cell_v2 = ZeroDModel.cell_voltage(ocv, loss, False)
+        cell_v1 = ZeroDModel._cell_voltage(ocv, loss, True)
+        cell_v2 = ZeroDModel._cell_voltage(ocv, loss, False)
 
         assert cell_v1 == 1.2
         assert cell_v2 == 0.8
